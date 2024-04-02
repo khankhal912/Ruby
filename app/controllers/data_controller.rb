54f915a44197434dfbb0fe89a515961a77dd16fa
khankhal912 <khankhaldhaval007@gmail.com>
2024-04-02 13:55:46 +0530

    minor change

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -8,11 +8,11 @@ class DataController < ApplicationController
     render('hello')
   end
 
-  # def hello1
-  #   redirect_to(:action => 'hello')
-  # end
+  def hello1
+    redirect_to(:action => 'hello')
+  end
 
-  # def youTube
-  #   redirect_to('https://youtu.be/rTgbdhc0Z3U?si=ufHF39vx_DTWxRR9',allow_other_host: true)
-  # end
+  def youTube
+    redirect_to('https://youtu.be/rTgbdhc0Z3U?si=ufHF39vx_DTWxRR9',allow_other_host: true)
+  end
 end
